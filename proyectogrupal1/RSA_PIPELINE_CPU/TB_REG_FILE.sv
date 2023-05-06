8996b98e5acaa709d543edcc05b4db22cd79657a
heutlett <44324449+Heutlett@users.noreply.github.com>
2023-05-06 13:27:59 -0600

    add: se actualiza el modulo de reg file para ser vectorial y se agrega testbench

---
+++ proyectogrupal1/RSA_PIPELINE_CPU/TB_REG_FILE.sv
@@ -0,0 +1,57 @@
+module TB_REG_FILE;
+
+	// Definición de las señales de entrada
+	logic clk;
+	logic WE3;
+	logic [3:0] A1;
+	logic [3:0] A2;
+	logic [3:0] A3;
+	logic [5:0][7:0] WD3;
+
+	// Definición de las señales de salida
+	logic [5:0][7:0] RD1;
+	logic [5:0][7:0] RD2;
+	logic [3:0] RD2I;
+
+	// Instanciar el módulo de registro
+	regfile regfile_inst (
+		.clk(clk),
+		.WE3(WE3),
+		.A1(A1),
+		.A2(A2),
+		.A3(A3),
+		.WD3(WD3),
+		.RD1(RD1),
+		.RD2(RD2),
+		.RD2I(RD2I)
+	);
+
+	 always begin
+	  clk = ~clk; #5;
+	 end
+
+	initial begin
+	
+		#10; 
+		clk = 0; #10;
+	
+		WE3 = 1;
+		A3 = 1;
+		WD3 = 'hABCD;
+		#10;
+		
+		WE3 = 1;
+		A3 = 2;
+		WD3 = 'h1234;
+		#10;
+	
+		A1 = 1;
+		A2 = 2;
+		#10;
+		$display("RD1 = %h", RD1);
+		$display("RD2 = %h", RD2);
+		$display("RD2I = %d", RD2I);
+		
+	end
+
+endmodule
--- proyectogrupal1/RSA_PIPELINE_CPU/regfile.sv
+++ proyectogrupal1/RSA_PIPELINE_CPU/regfile.sv
@@ -3,12 +3,12 @@ module regfile
 	// Entradas
 	input logic clk, WE3,
 	input logic [3:0] A1, A2, A3,
-	input logic [31:0] WD3,
+	input logic [5:0][7:0] WD3,
 	// Salidas
-	output logic [31:0] RD1, RD2
+	output logic [3:0] RD2I,
+	output logic [5:0][7:0] RD1, RD2
 );
-
-	logic [5:0][7:0] rf;
+	logic [5:0][7:0] rf[9:0];
 	
 	always_ff @(posedge clk) begin
 	
@@ -18,5 +18,6 @@ module regfile
 		
 	assign RD1 = rf[A1];
 	assign RD2 = rf[A2];
+	assign RD2I = A3;
 	
 endmodule
