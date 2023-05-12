15c91e845928445022b74fb0f24bfb8998ff56b1
Shakime Sparks <jeykimers.johnson@gmail.com>
2023-05-12 14:48:19 -0600

    add: top testbench

--- vectorial_cpu/VECTOR_CPU/TESTBENCHES/TB_TOP.sv
+++ vectorial_cpu/VECTOR_CPU/TESTBENCHES/TB_TOP.sv
@@ -1,37 +1,32 @@
 module TB_TOP();
 
-    // Señales de entrada
-    logic clk = 0;
-    logic reset = 1;
-    logic start = 0;
-    
-    // Señales de salida
-    logic EndFlag;
-	 
-	 
-	
+	// Entrada
+	logic clk;
+	logic reset;
+	logic start;
+
+	// Salida
+	logic EndFlag;
+
 	// Instanciación del módulo top
-	 top top_inst(
-		  .clk(clk),
-		  .reset(reset),
-		  .start(start),
-		  .EndFlag(EndFlag)
-	 );
+	top top_inst(
+		.clk(clk),
+		.reset(reset),
+		.start(start),
+		.EndFlag(EndFlag)
+	);
 	 
 	 
+	// initialize test
+	initial begin
+		clk = 0;
+		reset = 1; # 6; reset = 0;
+		start = 0; # 5; start = 1;
+	end
+
 	// generate clock to sequence tests
 	always begin
 		clk = ~clk; #5;
 	end
 	
-	// Señales de entrada
-	initial begin
-		#10; reset = 0; #10; reset = 1;
-		#10; start = 1; #10;
-		
-		
-		
-		#100 $finish;
-    end
-		
 endmodule
