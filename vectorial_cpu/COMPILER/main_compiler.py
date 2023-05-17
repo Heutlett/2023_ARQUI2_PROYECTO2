6b4f79711eb6a1456c3cbae62f7b29f89e8fc61b
Shakime Sparks <jeykimers.johnson@gmail.com>
2023-05-16 22:09:19 -0600

    add: bug compilador copiar archivo en MODULES

--- vectorial_cpu/COMPILER/main_compiler.py
+++ vectorial_cpu/COMPILER/main_compiler.py
@@ -1,10 +1,9 @@
-
 from shutil import copyfile
 from src.ClassCompiler import Compiler
 
 
 in_ = "program.asm"
-out_ = "../VECTOR_CPU/inst_mem_init.dat"
+out_ = "../VECTOR_CPU/MODULES/inst_mem_init.dat"
 compiler = Compiler(in_, out_)
 
 copyfile("inst_mem_init.dat", out_)
